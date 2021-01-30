7556a98377ff1c04c58f9b1415c8d933284ac18c
maithanhduyan <tiachop0102@gmail.com>
2021-01-31 00:56:06 +0900

    [UI-Fixed] Template

--- src/main/java/com/learnjapanese/controller/KanjiBasicController.java
+++ src/main/java/com/learnjapanese/controller/KanjiBasicController.java
@@ -11,15 +11,15 @@ import com.learnjapanese.service.KanjiBasicServiceImpl;
 @Controller
 public class KanjiBasicController {
 
-    @Autowired
-    private KanjiBasicServiceImpl kanjiService;
+	@Autowired
+	private KanjiBasicServiceImpl kanjiService;
 
-    @GetMapping("/214-bo-kanji-co-ban")
-    public String kanji214(
-	    @RequestParam(name = "name", required = false, defaultValue = "214 Bộ Thủ Kanji Cơ Bản") String name,
-	    Model model) {
-	model.addAttribute("name", name);
-	model.addAttribute("kanjiBasicList", kanjiService.findAll());
-	return "214_bo_Kanji_co_ban";
-    }
+	@GetMapping(value = { "/214-bo-kanji-co-ban", "/214-bo-kanji-co-ban.html" })
+	public String kanji214(
+			@RequestParam(name = "name", required = false, defaultValue = "214 Bộ Thủ Kanji Cơ Bản") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("kanjiBasicList", kanjiService.findAll());
+		return "214_bo_kanji_co_ban";
+	}
 }
