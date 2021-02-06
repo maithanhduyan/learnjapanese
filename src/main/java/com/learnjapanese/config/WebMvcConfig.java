947ca016ccff8fa2065230dd0dbcb67f1beac831
maithanhduyan <tiachop0102@gmail.com>
2021-02-06 13:31:16 +0900

    Add bootstrap 4 to lib.

--- src/main/java/com/learnjapanese/config/WebMvcConfig.java
+++ src/main/java/com/learnjapanese/config/WebMvcConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
-	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// Access Bootstrap static resource:
@@ -28,38 +27,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		// lib
 		registry.addResourceHandler("/lib/**") //
 				.addResourceLocations("classpath:/static/lib/");
-
-		// data
-		registry.addResourceHandler("/data/**") //
-				.addResourceLocations("classpath:/static/data/");
-
-		// http://somedomain/SomeContextPath/jquery/jquery.min.css
-		registry.addResourceHandler("/jquery/**") //
-				.addResourceLocations("classpath:/META-INF/resources/webjars/jquery/3.5.1/");
-
-		// http://somedomain/SomeContextPath/bootstrap/css/bootstrap.min.css
-		// http://somedomain/SomeContextPath/bootstrap/js/bootstrap.min.js
-		registry.addResourceHandler("/bootstrap/**") //
-				.addResourceLocations("classpath:/META-INF/resources/webjars/bootstrap/4.5.3/");
-
-		// http://somedomain/SomeContextPath/popper/popper.min.js
-		registry.addResourceHandler("/popper.js/**") //
-				.addResourceLocations("classpath:/META-INF/resources/webjars/popper.js/1.16.0/");
-
-		// http://somedomain/SomeContextPath/popper/popper.min.js
-		registry.addResourceHandler("/datatables/**") //
-				.addResourceLocations("classpath:/META-INF/resources/webjars/datatables/1.10.23/");
-
-		// http://somedomain/SomeContextPath/popper/popper.min.js
-		registry.addResourceHandler("/font-awesome/**") //
-				.addResourceLocations("classpath:/META-INF/resources/webjars/font-awesome/5.15.2/");
-
-		// websocket
-		registry.addResourceHandler("/stomp-websocket/**") //
-				.addResourceLocations("classpath:/META-INF/resources/webjars/stomp-websocket/2.3.3-1/");
-
-		registry.addResourceHandler("/sockjs-client/**") //
-				.addResourceLocations("classpath:/META-INF/resources/webjars/sockjs-client/1.1.2/");
 
 	}
 }
